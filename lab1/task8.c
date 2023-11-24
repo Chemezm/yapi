d1e1421dc01d7aaceaeacb393f842beb37363b67
Chemezm <151853932+Chemezm@users.noreply.github.com>
2023-11-24 14:26:19 +0500

    Add files via upload

---
+++ lab1/task8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	FILE *file = fopen("task8_1.txt", "r");
+	char info[100];
+
+	fgets(info,sizeof(info), stdin);
+
+	FILE * temp = fopen("temp.txt", "w");
+	fprintf(temp, "%s",info);
+
+	char c;
+	while ((c = fgetc(file))!=EOF){
+		fprintf(temp, "%c",c);
+	}
+	fclose(file);
+	fclose(temp);
+
+	remove("task8_1.txt");
+	rename("temp.txt","task8_1.txt");
+	return 0;
+}
